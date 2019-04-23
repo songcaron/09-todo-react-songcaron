be81ba437069a450cbfc30bcf3a5c1f805f8d6da
Caron Song <songcaron@wustl.edu>
2019-04-23 05:00:36 -0500

    Updates

---
+++ precache-manifest.fa37693c5703760b7526c66c37d57a00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2d9d039c363236c63b5",
+    "url": "/09-todo-react-songcaron/static/js/runtime~main.e2d9d039.js"
+  },
+  {
+    "revision": "32b753aed608b5e3197a",
+    "url": "/09-todo-react-songcaron/static/js/main.32b753ae.chunk.js"
+  },
+  {
+    "revision": "cd0319fc04b3dd1624b0",
+    "url": "/09-todo-react-songcaron/static/js/1.cd0319fc.chunk.js"
+  },
+  {
+    "revision": "32b753aed608b5e3197a",
+    "url": "/09-todo-react-songcaron/static/css/main.c022ea44.chunk.css"
+  },
+  {
+    "revision": "4b0a46fd192f460342c80623a89849f3",
+    "url": "/09-todo-react-songcaron/index.html"
+  }
+];
